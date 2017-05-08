6f67e488a84ed6aa1bd1002f0147009d0eddb3b5
XXX_pucie_slayer420_XXX <itay.gersten@gmail.com>
2017-05-08 22:42:51 +0300

    added the ui
    user interface

---
+++ app/src/main/java/com/example/user/glcdemoui/FontsOverride.java
@@ -0,0 +1,37 @@
+package com.example.user.glcdemoui;
+
+import android.graphics.Typeface;
+import android.icu.text.DateFormat;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by User on 04/04/2017.
+ */
+
+public final class FontsOverride {
+
+    public static void setDefaultFont(MainActivity context,
+                                      String staticTypefaceFieldName, String fontAssetName) {
+        final Typeface regular = Typeface.createFromAsset(context.getAssets(),
+                fontAssetName);
+        replaceFont(staticTypefaceFieldName, regular);
+    }
+
+    protected static void replaceFont(String staticTypefaceFieldName,
+                                      final Typeface newTypeface) {
+        try {
+            final Field staticField = Typeface.class
+                    .getDeclaredField(staticTypefaceFieldName);
+            staticField.setAccessible(true);
+
+            staticField.set(null, newTypeface);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
